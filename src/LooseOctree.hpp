4edfeeef2efe0db9c70e79e3da0cdf122fe819fc
Drwalin <marek.zalewski.drwalin@gmail.com>
2024-09-19 17:35:00 +0200

    More details for LooseOctree.

--- src/LooseOctree.hpp
+++ src/LooseOctree.hpp
@@ -2,35 +2,46 @@
 // Copyright (c) 2024 Marek Zalewski aka Drwalin
 // You should have received a copy of the MIT License along with this program.
 
+#include "../thirdparty/glm/glm/ext/vector_int3.hpp"
+
 #include "../include/spatial_partitioning/LooseOctree.hpp"
 
 namespace spp
 {
-LooseOctree::LooseOctree(spp::Aabb aabb, int32_t depth, float cellExtensionFactor) : aabb(aabb), depth(depth), cellExtensionFactor(cellExtensionFactor)
+LooseOctree::LooseOctree(glm::vec3 centerOffset, glm::vec3 sizeScale,
+		int32_t depth, float cellExtensionFactor)
+	: centerOffset(centerOffset), sizeScale(sizeScale), depth(depth), cellExtensionFactor(cellExtensionFactor),
+	halfTotalSize(1<<depth)
 {
+	Clear();
 }
 LooseOctree::~LooseOctree() {}
 
 const char *LooseOctree::GetName() const { return "LooseOctree"; }
 
-void LooseOctree::Clear() { entitiesData.clear(); }
+void LooseOctree::Clear()
+{
+	data.Clear();
+	nodes.Clear();
+	rootNode = nodes.Add({});
+}
 
 size_t LooseOctree::GetMemoryUsage() const
 {
-	return entitiesOffsets.bucket_count() * sizeof(void *) +
-		   entitiesOffsets.size() *
-			   (sizeof(void *) * 2lu + sizeof(uint32_t) + sizeof(EntityType)) +
-		   nodes.capacity() * sizeof(NodeData) +
-		   entitiesData.capacity() * sizeof(Data) +
-		   emptyNodesOffsets.capacity() * sizeof(int32_t);
+	return data.GetMemoryUsage() + nodes.GetMemoryUsage();
 }
 
-void LooseOctree::ShrinkToFit() {}
+void LooseOctree::ShrinkToFit() {
+	data.ShrinkToFit();
+	nodes.ShrinkToFit();
+}
 
 void LooseOctree::Add(EntityType entity, Aabb aabb, MaskType mask)
 {
-	entitiesData[entity] = Data{aabb, entity, mask};
-	_Internal_Add(entity, aabb);
+	int32_t offset = data.Add(entity, {aabb, entity, mask});
+	
+	// TODO: add to octree
+	
 	
 	
 	
@@ -39,16 +50,11 @@ void LooseOctree::Add(EntityType entity, Aabb aabb, MaskType mask)
 
 void LooseOctree::Update(EntityType entity, Aabb aabb)
 {
-	// TODO: optimise movement of objects
-	_Internal_RemoveNodes(entity);
-	entitiesData[entity].aabb = aabb;
-	_Internal_Add(entity, aabb);
+	// TODO: update
 }
 
 void LooseOctree::Remove(EntityType entity) {
-	_Internal_RemoveNodes(entity);
-	entitiesData.erase(entity);
-	
+	// TODO: remove
 	
 	
 	
@@ -56,25 +62,22 @@ void LooseOctree::Remove(EntityType entity) {
 
 void LooseOctree::SetMask(EntityType entity, MaskType mask)
 {
-	entitiesData[entity].mask = mask;
+	int32_t offset = data.GetOffset(entity);
+	data[offset].mask = mask;
+	
+	// TODO: update masks of tree
 }
 
 Aabb LooseOctree::GetAabb(EntityType entity) const
 {
-	auto it = entitiesData.find(entity);
-	if (it != entitiesData.end()) {
-		return it->second.aabb;
-	}
-	return {};
+	int32_t offset = data.GetOffset(entity);
+	return data[offset].aabb;
 }
 
 MaskType LooseOctree::GetMask(EntityType entity) const
 {
-	auto it = entitiesData.find(entity);
-	if (it != entitiesData.end()) {
-		return it->second.mask;
-	}
-	return 0;
+	int32_t offset = data.GetOffset(entity);
+	return data[offset].mask;
 }
 
 void LooseOctree::Rebuild() {}
@@ -135,5 +138,63 @@ void LooseOctree::IntersectRay(RayCallback &cb)
 			++cb.nodesTestedCount;
 		}
 	}
+}
+
+glm::vec3 LooseOctree::GetSizeOnDepth(int32_t depth) const
+{
+	const int32_t v = 1 << (this->depth - depth);
+	return glm::vec3(v, v, v) * cellExtensionFactor;
+}
+
+int32_t LooseOctree::GetChildIdFromCenter(glm::vec3 p) const
+{
+	int32_t id = 0;
+	for (int i=0; i<3; ++i) {
+		id += p[i] > 0 ? 1<<i : 0;
+	}
+	return id;
+}
+
+glm::vec3 LooseOctree::GetPosOffsetOfNodeAtDepth(int32_t depth,
+												 int32_t childId) const
+{
+	const int32_t b = 1 << (this->depth - depth);
+	const glm::vec3 hs = glm::vec3(b, b, b) * 0.5f;
+	const glm::vec3 extensinoOffset = hs * (cellExtensionFactor - 1.0f);
+
+	const glm::vec3 bs = {childId & 1 ? 1.0f : 0.0f, childId & 2 ? 1.0f : 0.0f,
+						  childId & 4 ? 1.0f : 0.0f};
+
+	return extensinoOffset + bs * hs;
+}
+
+LooseOctree::NodePath LooseOctree::CalculatePathTo(Aabb aabb)
+{
+	aabb.min -= centerOffset;
+	const glm::vec3 size = aabb.GetSizes() * sizeScale;
+	aabb.max = aabb.min + size;
+	const glm::ivec3 icenter = aabb.GetCenter();
+	const float maxSizeValue = glm::max(glm::max(size.x, size.y), size.z);
+	
+	NodePath path;
+	
+	const int32_t maxDepth = this->depth - std::ilogb(maxSizeValue);
+	path.depth = 0;
+	for (int32_t &i=path.depth; i<depth; ++i) {
+// 		int32_t g = 
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
 } // namespace spp
